c62df539a712839409b05f23453a074eaa9316f7
thedeveren-collab <145158532+thedeveren-collab@users.noreply.github.com>
2023-09-16 07:57:26 +0300

    Create base-product-whitelist.lua

---
+++ base-product-whitelist.lua
@@ -0,0 +1,62 @@
+-- Base Whitelist
+
+return {
+	Whitelist = {
+		[1518941214] = {
+			-- co-owner of assets
+			products = {
+				[111] = "AdBoard V1", -- No whitelisting system
+				[112] = "AdBoard V1 Premium", -- No whitelisting system
+				[142] = "PlaylistScript V1.4",
+			},
+		},
+		[1816895222] = {
+			-- owner of assets
+			products = {
+				[111] = "AdBoard V1", -- No whitelisting system
+				[112] = "AdBoard V1 Premium", -- No whitelisting system
+				[142] = "PlaylistScript V1.4",
+			},
+		},
+		[2226736919] = {
+			products = {
+				[111] = "AdBoard V1", -- No whitelisting system
+				[112] = "AdBoard V1 Premium", -- No whitelisting system
+				[142] = "PlaylistScript V1.4",
+			},
+		},
+		[3125837320] = {
+			products = {
+				[142] = "PlaylistScript V1.4",
+			},
+		},
+		[3456028202] = {
+			products = {
+				[142] = "PlaylistScript V1.4",
+			},
+		},
+		[4501715995] = {
+			products = {
+				[142] = "PlaylistScript V1.4",
+			},
+		},
+	},
+	Blacklist = {
+		[3177378056] = {
+			products = {
+				[111] = "AdBoard V1", -- No whitelisting system
+				[112] = "AdBoard V1 Premium", -- No whitelisting system
+				[142] = "PlaylistScript V1.4",
+			},
+			reason = "Asset thiefing is not allowed on E™ - Join the Discoard Server for more information. .gg/U2ybCPxU57",
+		},
+		[4444578446] = {
+			products = {
+				[111] = "AdBoard V1", -- No whitelisting system
+				[112] = "AdBoard V1 Premium", -- No whitelisting system
+				[142] = "PlaylistScript V1.4",
+			},
+			reason = "Threatening to steal assets.",
+		},
+	}
+}
