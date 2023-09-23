0e3a457130879c0a68e3eb0d9f9c6ba84ce6b5c0
thedeveren-collab <145158532+thedeveren-collab@users.noreply.github.com>
2023-09-24 00:32:47 +0300

    Update base-product-whitelist.lua

--- base-staffing-modules/base-product-whitelist.lua
+++ base-staffing-modules/base-product-whitelist.lua
@@ -1,12 +1,5 @@
 return {
 	Whitelist = {
-        [!sendfrombot] = {
-            products = {
-                [111] = "AdBoard V1", -- No whitelisting system
-                [112] = "AdBoard V1 Premium", -- No whitelisting system
-                [142] = "PlaylistScript V1.4",
-            },
-        },
 		[1518941214] = {
 			-- co-owner of assets
 			products = {
