be83b1fc77f0cbfa2384ef12bd9ae754900350a8
thedeveren-collab <145158532+thedeveren-collab@users.noreply.github.com>
2023-10-20 17:08:58 +0300

    Updated by bot

--- base-staffing-modules/base-product-whitelist.lua
+++ base-staffing-modules/base-product-whitelist.lua
@@ -1,5 +1,19 @@
 return {
 	Whitelist = {
+        [1518941214] = {
+            products = {
+                [111] = "AdBoard V1", -- No whitelisting system
+                [112] = "AdBoard V1 Premium", -- No whitelisting system
+                [142] = "PlaylistScript V1.4",
+            },
+        },
+        [1518941214] = {
+            products = {
+                [111] = "AdBoard V1", -- No whitelisting system
+                [112] = "AdBoard V1 Premium", -- No whitelisting system
+                [142] = "PlaylistScript V1.4",
+            },
+        },
         [4791751886] = {
             products = {
                 [111] = "AdBoard V1", -- No whitelisting system
